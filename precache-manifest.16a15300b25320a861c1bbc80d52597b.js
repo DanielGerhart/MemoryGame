b14b14209e6aa34b68d33eb292db26a72b5d2169
DanielGerhart <djgerhart@outlook.com>
2019-11-15 08:33:51 -0600

    Updates

---
+++ precache-manifest.16a15300b25320a861c1bbc80d52597b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9a3e6d88305831a63b3083606f365f",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "d0bc41e1fc0df426291b",
+    "url": "/MemoryGame/static/css/main.4018408f.chunk.css"
+  },
+  {
+    "revision": "93d7c0073a6766b07ab2",
+    "url": "/MemoryGame/static/js/2.dae3e278.chunk.js"
+  },
+  {
+    "revision": "d0bc41e1fc0df426291b",
+    "url": "/MemoryGame/static/js/main.3a0a1736.chunk.js"
+  },
+  {
+    "revision": "d0ef06665aff611ef971",
+    "url": "/MemoryGame/static/js/runtime-main.be30467c.js"
+  }
+]);
